b6b716720facb945969faab695c675c052c6c286
Will.i.am <will@i.am>
2013-07-09 17:57:51 +0000

    Use a local buffer instead of a global buffer

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 
-var buf = new Buffer(fs.readFileSync('index.html'));
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buf = new Buffer(fs.readFileSync('index.html'));
   response.send(buf.toString());
 });
 
