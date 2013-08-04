fbd64fa26f18357ff4537c9e6b6184af5a2a1fc5
William Bong <will@i.am>
2013-08-04 14:54:02 +0000

    Buffer buf replaced with var htmlfile. See Figure 15 of startup-lecture_slides-lecture7-deployment-dns-custom-domains.pdf

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = new Buffer(fs.readFileSync('index.html'));
-  response.send(buf.toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 8080;
